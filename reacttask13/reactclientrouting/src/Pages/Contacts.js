39370d1abd9cb03981c2920d6f938bb86bd5b419
elnurrahimov <105945571+elnurrahimov@users.noreply.github.com>
2022-12-05 21:33:51 +0400

    Add files via upload

---
+++ reacttask13/reactclientrouting/src/Pages/Contacts.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contacts() {
+  return (
+    <div className="mt-5">
+      <h1 className="text-center font-bold text-3xl">Contacts</h1>
+      <p className="ml-4 mt-4">Phone number:xxxxxxxxx <br></br>
+      Email: elnurrahimov17@gmail.com</p>
+    </div>
+  );
+}
+
+export default Contacts;
